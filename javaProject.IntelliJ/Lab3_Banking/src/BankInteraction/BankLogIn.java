84a8e61aa311268b1c8d355d8a663ff8a5d2c217
Simon Magnusson <masi23rc@student.ju.se>
2025-05-02 09:24:13 +0200

    completed lab3_Banking

--- javaProject.IntelliJ/Lab3_Banking/src/BankInteraction/BankLogIn.java
+++ javaProject.IntelliJ/Lab3_Banking/src/BankInteraction/BankLogIn.java
@@ -4,49 +4,97 @@ import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class BankLogIn extends JFrame implements ActionListener {
+public class BankLogIn extends JFrame{
 
     private final JButton cancel;
     private final JButton ok;
+    private final JTextField usernameText;
+    private final JPasswordField passwordText;
+    private int loginAttempts = 0;
 
 
     public BankLogIn(){
-        JFrame frame = new JFrame();
-        cancel = new JButton("Cancel");
+        //Create basic login Frame//
+        super("Bank Login");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(450,250);
+        setLayout(null);
+
+        //2 Buttons: Cancel and OK//
         ok = new JButton("OK");
-
-
         ok.setBounds(130,150,90,40);
         ok.setFocusable(false);
-        ok.addActionListener((ActionListener) this);
-        frame.add(ok);
-
+        cancel = new JButton("Cancel");
         cancel.setBounds(230,150,90,40);
         cancel.setFocusable(false);
-        cancel.addActionListener((ActionListener) this);
-        frame.add(cancel);
+        add(ok);
+        add(cancel);
 
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(450,250);
-        frame.setLayout(null);
-
-        JLabel label = new JLabel("Bank Log In");
-        label.setHorizontalAlignment(SwingConstants.LEFT);
-        label.setVerticalAlignment(SwingConstants.TOP);
-        frame.add(label);
-
-
-       // JOptionPane.showInternalOptionDialog(null,"password","Login",JOptionPane.OK_CANCEL_OPTION,JOptionPane.INFORMATION_MESSAGE,null,null,0);
+        //User\password textFields//
+        usernameText = new JTextField("");
+        usernameText.setBounds(230,60,120,25);
+        passwordText = new JPasswordField("");
+        passwordText.setBounds(230,90,120,25);
+        add(usernameText);
+        add(passwordText);
 
 
-        frame.setVisible(true);
+        //User\password labels//
+        JLabel usernameLabel = new JLabel("Username: ");
+        usernameLabel.setBounds(130,60,120,25);
+        JLabel passwordLabel = new JLabel("Password: ");
+        passwordLabel.setBounds(130,90,120,25);
+        add(usernameLabel);
+        add(passwordLabel);
+
+        //Images//
+        ImageIcon login = new ImageIcon("LoginMan.png");
+        ImageIcon loginFrame = new ImageIcon("LoginIcon.jpg");
+        setIconImage(login.getImage());
+        JLabel imageLabel = new JLabel(loginFrame);
+        imageLabel.setBounds(15,35,100,100);
+        add(imageLabel);
+
+        //Button Handler//
+        LoginHandler loginHandler = new LoginHandler();
+        ok.addActionListener(loginHandler);
+        cancel.addActionListener(loginHandler);
+
+        //Set enter key to press OK button//
+        getRootPane().setDefaultButton(ok);
+        setVisible(true);
     }
 
-    @Override
-    public void actionPerformed(ActionEvent e){
-        if(e.getSource() == ok)
-            System.out.println("OK Button clicked");
-        else if(e.getSource() == cancel)
-            System.out.println("Cancel Button clicked");
+    public class LoginHandler implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            if (e.getSource() == ok) {
+                String username = "user123";
+                String password = "1223334444";
+                if (usernameText.getText().equals(username) &&
+                        String.valueOf(passwordText.getPassword()).equals(password)){
+                    BankLogIn.this.dispose();
+                    BankGUI bank = new BankGUI();
+                } else{
+                    ++loginAttempts;
+                    if(loginAttempts < 3) {
+                        JOptionPane.showMessageDialog(
+                                BankLogIn.this,
+                                "Invalid Username or Password",
+                                "Login Failed",
+                                JOptionPane.ERROR_MESSAGE);
+                    }else if(loginAttempts == 3){
+                        JOptionPane.showMessageDialog(
+                                BankLogIn.this,
+                                "Too many failed attempts, Program will exit",
+                                "Access Denied",
+                                JOptionPane.ERROR_MESSAGE);
+                        BankLogIn.this.dispose();
+                    }
+                }
+            } else if (e.getSource() == cancel) {
+                BankLogIn.this.dispose();
+            }
+        }
     }
 }
